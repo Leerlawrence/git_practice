843c4ebb9bf3c863e2a23c397a30ee07b8000425
Kiyo <kiyo921@hotmail.co.jp>
2016-10-05 10:47:55 +0100

    solved merge conflict

--- lees.js
+++ lees.js
@@ -1,15 +1,3 @@
 hello kiyoh - do not change me
 
-<<<<<<< HEAD
 figaro magnifico
-=======
-Sachuma
-Sachuma
-Sachuma
-Sachuma
-Sachuma
-Sachuma
-Sachuma
-Sachuma
-Sachuma
->>>>>>> 7d32672994e93519ebab4d296d34e12570b58b83
