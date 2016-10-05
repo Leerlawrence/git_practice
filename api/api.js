d72c7764f7073b2cf9511aa7513930e3ccf642cb
Lee Lawrence <leerlawrence@gmail.com>
2016-10-05 13:17:16 +0100

    api working

--- api/api.js
+++ api/api.js
@@ -1,5 +1,9 @@
 var FruitApi = function(app){
 
+  var fruits=["Apple","Orange","Lychee", "Kumquat"];
+  app.get("/fruits",function(req,res){    
+    res.json({data:fruits});
+    console.log(res);
+  });
 }
-
 module.exports = FruitApi;
